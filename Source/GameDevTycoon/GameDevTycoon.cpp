2406a311221b0284b37dff76e5725d64c44c8de5
goblinincident <goblinincident@gmail.com>
2015-04-01 16:08:50 +1100

    STATE: base project

---
+++ Source/GameDevTycoon/GameDevTycoon.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GameDevTycoon.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GameDevTycoon, "GameDevTycoon" );
---
+++ Source/GameDevTycoon/GameDevTycoonGameMode.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GameDevTycoon.h"
+#include "GameDevTycoonGameMode.h"
+
+
+
+
